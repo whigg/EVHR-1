300c6d2b09ebb4d1969185bdc5e6b9f652d411e8
a <a@gs619-rgillm1.local>
2018-04-12 11:01:03 -0400

    tiler-class

--- EvhrEngine/management/EvhrMosaicRetriever.py
+++ EvhrEngine/management/EvhrMosaicRetriever.py
@@ -45,6 +45,9 @@ from django.conf import settings
 #
 # --- Crystal Fire ---
 # ./manage.py processGeoRequest --name testCrystal --epName "EVHR Mosaic" --ulx -113.39250146 --uly 43.35041085 --lrx -112.80953835 --lry 42.93059617 --epsg 4326 --outEpsg 102039 -n 1
+#
+# --- Fairbanks ---
+# ./manage.py processGeoRequest --name testCrystal --epName "EVHR Mosaic" --ulx -148 --uly 65 --lrx -147.5 --lry 64.5 --epsg 4326 --outEpsg 4326 -n 1
 #-------------------------------------------------------------------------------
 class EvhrMosaicRetriever(GeoRetriever):
 
@@ -343,11 +346,18 @@ class EvhrMosaicRetriever(GeoRetriever):
         sceneGeoms = {}
         
         for scene in scenes:
-            
-            dg = DgFile(scene)
-            geom = self.bBoxToPolygon(dg.ulx, dg.uly, dg.lrx, dg.lry,dg.srs)
-            sceneGeoms[scene] = geom
-                                      
+        
+            try:
+                
+                dg = DgFile(scene)
+                geom = self.bBoxToPolygon(dg.ulx, dg.uly, dg.lrx, dg.lry,dg.srs)
+                sceneGeoms[scene] = geom
+
+            except Exception, e:
+                
+                msg = e.msg + ' Scene: ' + str(scene)
+                raise RuntimeError(msg)
+                
         # Define the tiles.
         tiler = TilerHalfDegree(self.retrievalUlx,
                                 self.retrievalUly,
