0e6a11d5e05a74de3e42d6613719ccae35f2c1d3
a <a@gs619-rgillm1.local>
2018-05-09 15:00:41 -0400

    test-api

--- api/urls.py
+++ api/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^getOutput/$',          api.views.download),
     url(r'^orderMosaic/$',        api.views.orderMosaic),
     url(r'^percentageComplete/$', api.views.percentageComplete),
-    url(r'^ping',                 api.views.ping),
+    url(r'^ping/$',               api.views.ping),
     url(r'^status/$',             api.views.status),
 ]
