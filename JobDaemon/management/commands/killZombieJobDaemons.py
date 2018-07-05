a71bd308d04dad2662ebcafe4001929329a86b2a
a <a@gs619-rgillm1.local>
2018-07-05 15:31:48 -0400

    log-files

---
+++ JobDaemon/management/commands/killZombieJobDaemons.py
@@ -0,0 +1,55 @@
+
+import os
+
+from django.core.management.base import BaseCommand
+
+from ProcessingEngine.models import ConstituentProcess
+from JobDaemon.models import JobDaemonProcess
+
+#------------------------------------------------------------------------
+# Command
+#
+# manage.py killZombieJobDaemons
+#------------------------------------------------------------------------
+class Command(BaseCommand):
+    
+    #--------------------------------------------------------------------
+    # handle
+    #--------------------------------------------------------------------
+    def handle(self, **options):
+    
+        # Get all the process IDs.
+        pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+
+        for pid in pids:
+            
+            try:
+            
+                # Does it have 'jobDaemon' in the command line?
+                cmdLine = open(os.path.join('/proc', pid, 'cmdline'), 'rb').read()
+            
+                if cmdLine.find('jobDaemon') != -1:
+                    
+                    #---
+                    # If this PID is unrepresented in ConstituentProcess
+                    # or JobDaemonProcess, delete it.
+                    #---
+                    if JobDaemonProcess.objects.filter(pid = pid).exists():
+                        continue
+                        
+                    if ConstituentProcess.objects.filter(pid = pid).exists():
+                        continue
+                        
+                    # Delete the PID.
+                    print 'Killing JobDaemon process ' + str(pid) + '...'
+                    os.kill(int(pid), 9)
+                    
+            except OSError: # probably don't have permission to kill
+
+                print 'Unable to kill ' + str(pid)
+                continue
+                
+            except IOError: # proc has already terminated
+                continue
+        
+
--- JobDaemon/management/commands/purgeRequestDirs.py
+++ JobDaemon/management/commands/purgeRequestDirs.py
@@ -1,6 +1,5 @@
 
 import glob
-import os
 import shutil
 
 from django.conf import settings
@@ -8,44 +7,31 @@ from django.core.management.base import BaseCommand
 
 from ProcessingEngine.models import Request
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------
 # Command
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------
 class Command(BaseCommand):
 
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------
     # handle
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------
     def handle(self, **options):
     
         purgeRequestDirs()
         
-#-------------------------------------------------------------------------------
+#--------------------------------------------------------------------
 # purgeReqDirs
-#-------------------------------------------------------------------------------
+#--------------------------------------------------------------------
 def purgeRequestDirs():
 
         workDir = settings.WORK_DIRECTORY
         onDisk  = glob.glob(workDir + '/*')
         inDb    = Request.objects.values_list('destination', flat = True)
-        notInDb = []
-        
-        for diskFile in onDisk:
+        notInDb = [d for d in onDisk if d not in inDb]
 
-            if not os.path.isdir(diskFile):
-                continue
-
-            found = False
-            
-            for dbFile in inDb:
-            
-                if diskFile == dbFile or diskFile == os.path.split(dbFile)[0]:
-            
-                    found = True
-                    break
-                        
-            if not found:
-                
-                print 'Removing orphaned directory: ' + str(diskFile)
-                shutil.rmtree(diskFile)
-
+        print 'Orphaned request directories to be deleted: ' + str(notInDb)
+    
+        for d in notInDb:
+            shutil.rmtree(d)
+    
+    
