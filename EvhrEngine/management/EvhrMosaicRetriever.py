97b3c6babec778bcf3945facc93b3bc5d2732ff6
a <a@gs619-rgillm1.local>
2018-08-08 11:18:34 -0400

    EvhrHelper

--- EvhrEngine/management/EvhrMosaicRetriever.py
+++ EvhrEngine/management/EvhrMosaicRetriever.py
@@ -6,7 +6,7 @@ import os
 import shutil
 import tempfile
 import traceback
-from xml.dom import minidom
+# from xml.dom import minidom
 
 import numpy
 
@@ -22,7 +22,7 @@ from EvhrEngine.management.SystemCommand import SystemCommand
 from EvhrEngine.management.TilerHalfDegree import TilerHalfDegree
 from EvhrEngine.management.commands.TOA import TOA
 from EvhrEngine.models import EvhrError
-from EvhrEngine.models import EvhrScene
+# from EvhrEngine.models import EvhrScene
 
 #-------------------------------------------------------------------------------
 # class EvhrMosaicRetriever
@@ -79,7 +79,7 @@ class EvhrMosaicRetriever(GeoRetriever):
         if not self.retrievalSRS.IsSame(GeoRetriever.GEOG_4326):
             raise RuntimeError('Retrieval SRS must be geographic.')
 
-        self.runSensors = ['WV01', 'WV02', 'WV03']
+        # self.runSensors = ['WV01', 'WV02', 'WV03']
 
         # Ensure the ortho and toa directories exist.
         self.tileDir  = os.path.join(self.request.destination.name, '1-tiles')
@@ -103,54 +103,54 @@ class EvhrMosaicRetriever(GeoRetriever):
         if not os.path.exists(self.toaDir):
             os.mkdir(self.toaDir)
 
-    #---------------------------------------------------------------------------
-    # clipShp
+    # #---------------------------------------------------------------------------
+    # # clipShp
+    # #
+    # # listConstituents -> getScenes -> queryFootprints -> clipShp
+    # #
+    # # retrieveOne -> processScene -> orthoOne -> createDemForOrthos
+    # # -> mosaicAndClipDemTiles -> clipShp
+    # #---------------------------------------------------------------------------
+    # def clipShp(self, shpFile, ulx, uly, lrx, lry, srs, extraQueryParams = ''):
     #
-    # listConstituents -> queryFootprints -> clipShp
+    #     if self.logger:
+    #         self.logger.info('Clipping Shapefile.')
     #
-    # retrieveOne -> processScene -> orthoOne -> createDemForOrthos
-    # -> mosaicAndClipDemTiles -> clipShp
-    #---------------------------------------------------------------------------
-    def clipShp(self, shpFile, ulx, uly, lrx, lry, srs, extraQueryParams = ''):
-
-        if self.logger:
-            self.logger.info('Clipping Shapefile.')
-
-        # Create a temporary file for the clip output.
-        tempClipFile = tempfile.mkstemp()[1]
-        
-        #---
-        # To filter scenes that only overlap the AoI slightly, decrease both
-        # corners of the query AoI.
-        #---
-        MIN_OVERLAP_IN_DEGREES = 0.2
-        ulx = float(ulx) + MIN_OVERLAP_IN_DEGREES
-        uly = float(uly) - MIN_OVERLAP_IN_DEGREES
-        lrx = float(lrx) - MIN_OVERLAP_IN_DEGREES
-        lry = float(lry) + MIN_OVERLAP_IN_DEGREES
-
-        # Clip.  The debug option somehow prevents an occasional seg. fault!
-        cmd = 'ogr2ogr'                        + \
-              ' -f "GML"'                      + \
-              ' -spat'                         + \
-              ' ' + str(ulx)                   + \
-              ' ' + str(lry)                   + \
-              ' ' + str(lrx)                   + \
-              ' ' + str(uly)                   + \
-              ' -spat_srs'                     + \
-              ' "' + srs.ExportToProj4() + '"' + \
-              ' --debug on'                    + \
-              ' ' + str(extraQueryParams)      + \
-              ' "' + tempClipFile + '"'        + \
-              ' "' + shpFile + '"'
-
-        sCmd = SystemCommand(cmd, shpFile, self.logger, self.request, True)
-
-        xml      = minidom.parse(tempClipFile)
-        features = xml.getElementsByTagName('gml:featureMember')
-
-        return features
-
+    #     # Create a temporary file for the clip output.
+    #     tempClipFile = tempfile.mkstemp()[1]
+    #
+    #     #---
+    #     # To filter scenes that only overlap the AoI slightly, decrease both
+    #     # corners of the query AoI.
+    #     #---
+    #     MIN_OVERLAP_IN_DEGREES = 0.2
+    #     ulx = float(ulx) + MIN_OVERLAP_IN_DEGREES
+    #     uly = float(uly) - MIN_OVERLAP_IN_DEGREES
+    #     lrx = float(lrx) - MIN_OVERLAP_IN_DEGREES
+    #     lry = float(lry) + MIN_OVERLAP_IN_DEGREES
+    #
+    #     # Clip.  The debug option somehow prevents an occasional seg. fault!
+    #     cmd = 'ogr2ogr'                        + \
+    #           ' -f "GML"'                      + \
+    #           ' -spat'                         + \
+    #           ' ' + str(ulx)                   + \
+    #           ' ' + str(lry)                   + \
+    #           ' ' + str(lrx)                   + \
+    #           ' ' + str(uly)                   + \
+    #           ' -spat_srs'                     + \
+    #           ' "' + srs.ExportToProj4() + '"' + \
+    #           ' --debug on'                    + \
+    #           ' ' + str(extraQueryParams)      + \
+    #           ' "' + tempClipFile + '"'        + \
+    #           ' "' + shpFile + '"'
+    #
+    #     sCmd = SystemCommand(cmd, shpFile, self.logger, self.request, True)
+    #
+    #     xml      = minidom.parse(tempClipFile)
+    #     features = xml.getElementsByTagName('gml:featureMember')
+    #
+    #     return features
+    #
     #---------------------------------------------------------------------------
     # compress
     #
@@ -288,40 +288,42 @@ class EvhrMosaicRetriever(GeoRetriever):
     def getEndPointSRSs(self, endPoint):
         return [GeoRetriever.GEOG_4326]
 
-    #---------------------------------------------------------------------------
-    # getScenes
-    #---------------------------------------------------------------------------
-    def getScenes(self):
-
-        # Check if there are already scenes associated with this request.
-        evhrScenes = EvhrScene.objects.filter(request = self.request)
-        scenes = []
-
-        if evhrScenes:
-            
-            for es in evhrScenes:
-                scenes.append(es.sceneFile.name)
-
-        else:
-            
-            MAX_FEATS = 100
-
-            # AoI + FOOTPRINTS = scenes
-            scenes = self.queryFootprints(self.retrievalUlx,
-                                          self.retrievalUly,
-                                          self.retrievalLrx,
-                                          self.retrievalLry,
-                                          self.retrievalSRS,
-                                          MAX_FEATS)
-                                          
-            for scene in scenes:
-                
-                evhrScene = EvhrScene()
-                evhrScene.request = self.request
-                evhrScene.sceneFile = scene
-                evhrScene.save()
-                
-        return scenes
+    # #---------------------------------------------------------------------------
+    # # getScenes
+    # #
+    # # listConstituents -> getScenes
+    # #---------------------------------------------------------------------------
+    # def getScenes(self):
+    #
+    #     # Check if there are already scenes associated with this request.
+    #     evhrScenes = EvhrScene.objects.filter(request = self.request)
+    #     scenes = []
+    #
+    #     if evhrScenes:
+    #
+    #         for es in evhrScenes:
+    #             scenes.append(es.sceneFile.name)
+    #
+    #     else:
+    #
+    #         MAX_FEATS = 100
+    #
+    #         # AoI + FOOTPRINTS = scenes
+    #         scenes = self.queryFootprints(self.retrievalUlx,
+    #                                       self.retrievalUly,
+    #                                       self.retrievalLrx,
+    #                                       self.retrievalLry,
+    #                                       self.retrievalSRS,
+    #                                       MAX_FEATS)
+    #
+    #         for scene in scenes:
+    #
+    #             evhrScene = EvhrScene()
+    #             evhrScene.request = self.request
+    #             evhrScene.sceneFile = scene
+    #             evhrScene.save()
+    #
+    #     return scenes
                 
     #---------------------------------------------------------------------------
     # getUtmSrs
@@ -719,51 +721,51 @@ class EvhrMosaicRetriever(GeoRetriever):
                 
         return toaFinal
 
-    #---------------------------------------------------------------------------
-    # queryFootprints
+    # #---------------------------------------------------------------------------
+    # # queryFootprints
+    # #
+    # # listConstituents -> getScenes -> queryFootprints
+    # #---------------------------------------------------------------------------
+    # def queryFootprints(self, ulx, uly, lrx, lry, srs, maxFeatures = None):
     #
-    # listConstituents -> queryFootprints
-    #---------------------------------------------------------------------------
-    def queryFootprints(self, ulx, uly, lrx, lry, srs, maxFeatures = None):
-
-        whereClause = '-where "'
-        first = True
-
-        for sensor in self.runSensors:
-
-            if first:
-                first = False
-            else:
-                whereClause += ' OR '
-
-            whereClause += 'SENSOR=' + "'" + sensor + "'"
-
-        whereClause += '"'
-
-        features = self.clipShp(EvhrMosaicRetriever.FOOTPRINTS_FILE, \
-                                ulx, uly, lrx, lry, srs,             \
-                                whereClause)
-
-        # Put them into a list of (row, path) tuples.
-        nitfs = []
-        featureCount = 0
-
-        for feature in features:
-
-            featureCount += 1
-
-            if maxFeatures and featureCount > maxFeatures:
-                break
-
-            nitf = str(feature. \
-                       getElementsByTagName('ogr:S_FILEPATH')[0]. \
-                       firstChild. \
-                       data)
-
-            nitfs.append(nitf)
-
-        return nitfs
-
+    #     whereClause = '-where "'
+    #     first = True
+    #
+    #     for sensor in EvhrHelper.RUN_SENSORS:
+    #
+    #         if first:
+    #             first = False
+    #         else:
+    #             whereClause += ' OR '
+    #
+    #         whereClause += 'SENSOR=' + "'" + sensor + "'"
+    #
+    #     whereClause += '"'
+    #
+    #     features = self.clipShp(EvhrMosaicRetriever.FOOTPRINTS_FILE, \
+    #                             ulx, uly, lrx, lry, srs,             \
+    #                             whereClause)
+    #
+    #     # Put them into a list of (row, path) tuples.
+    #     nitfs = []
+    #     featureCount = 0
+    #
+    #     for feature in features:
+    #
+    #         featureCount += 1
+    #
+    #         if maxFeatures and featureCount > maxFeatures:
+    #             break
+    #
+    #         nitf = str(feature. \
+    #                    getElementsByTagName('ogr:S_FILEPATH')[0]. \
+    #                    firstChild. \
+    #                    data)
+    #
+    #         nitfs.append(nitf)
+    #
+    #     return nitfs
+    #
     #---------------------------------------------------------------------------
     # retrieveOne
     #
