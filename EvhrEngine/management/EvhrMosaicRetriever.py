050a281d6f27d497c4d916e3e1935b1e05074bc7
a <a@gs619-rgillm1.local>
2018-08-13 11:04:15 -0400

    georetriever-static

--- EvhrEngine/management/EvhrMosaicRetriever.py
+++ EvhrEngine/management/EvhrMosaicRetriever.py
@@ -66,8 +66,10 @@ class EvhrMosaicRetriever(GeoRetriever):
     #---------------------------------------------------------------------------
     def __init__(self, request, logger, numProcesses):
 
+        self.evhrHelper = EvhrHelper(logger)
+
         # The output SRS must be UTM, regardless of what the user chooses.
-        request.outSRS = self.getUtmSrs(request)
+        request.outSRS = evhrHelper.getUtmSrs(request)
         request.save(update_fields = ['outSRS'])
 
         # Initialize the base class.
@@ -104,56 +106,6 @@ class EvhrMosaicRetriever(GeoRetriever):
         if not os.path.exists(self.toaDir):
             os.mkdir(self.toaDir)
             
-        self.evhrHelper = EvhrHelper(logger)
-
-    # #---------------------------------------------------------------------------
-    # # clipShp
-    # #
-    # # listConstituents -> getScenes -> queryFootprints -> clipShp
-    # #
-    # # retrieveOne -> processScene -> orthoOne -> createDemForOrthos
-    # # -> mosaicAndClipDemTiles -> clipShp
-    # #---------------------------------------------------------------------------
-    # def clipShp(self, shpFile, ulx, uly, lrx, lry, srs, extraQueryParams = ''):
-    #
-    #     if self.logger:
-    #         self.logger.info('Clipping Shapefile.')
-    #
-    #     # Create a temporary file for the clip output.
-    #     tempClipFile = tempfile.mkstemp()[1]
-    #
-    #     #---
-    #     # To filter scenes that only overlap the AoI slightly, decrease both
-    #     # corners of the query AoI.
-    #     #---
-    #     MIN_OVERLAP_IN_DEGREES = 0.2
-    #     ulx = float(ulx) + MIN_OVERLAP_IN_DEGREES
-    #     uly = float(uly) - MIN_OVERLAP_IN_DEGREES
-    #     lrx = float(lrx) - MIN_OVERLAP_IN_DEGREES
-    #     lry = float(lry) + MIN_OVERLAP_IN_DEGREES
-    #
-    #     # Clip.  The debug option somehow prevents an occasional seg. fault!
-    #     cmd = 'ogr2ogr'                        + \
-    #           ' -f "GML"'                      + \
-    #           ' -spat'                         + \
-    #           ' ' + str(ulx)                   + \
-    #           ' ' + str(lry)                   + \
-    #           ' ' + str(lrx)                   + \
-    #           ' ' + str(uly)                   + \
-    #           ' -spat_srs'                     + \
-    #           ' "' + srs.ExportToProj4() + '"' + \
-    #           ' --debug on'                    + \
-    #           ' ' + str(extraQueryParams)      + \
-    #           ' "' + tempClipFile + '"'        + \
-    #           ' "' + shpFile + '"'
-    #
-    #     sCmd = SystemCommand(cmd, shpFile, self.logger, self.request, True)
-    #
-    #     xml      = minidom.parse(tempClipFile)
-    #     features = xml.getElementsByTagName('gml:featureMember')
-    #
-    #     return features
-   
     #---------------------------------------------------------------------------
     # compress
     #
@@ -291,174 +243,46 @@ class EvhrMosaicRetriever(GeoRetriever):
     def getEndPointSRSs(self, endPoint):
         return [GeoRetriever.GEOG_4326]
 
-    # #---------------------------------------------------------------------------
-    # # getScenes
-    # #
-    # # listConstituents -> getScenes
-    # #---------------------------------------------------------------------------
-    # def getScenes(self):
-    #
-    #     # Check if there are already scenes associated with this request.
-    #     evhrScenes = EvhrScene.objects.filter(request = self.request)
-    #     scenes = []
-    #
-    #     if evhrScenes:
-    #
-    #         for es in evhrScenes:
-    #             scenes.append(es.sceneFile.name)
-    #
-    #     else:
-    #
-    #         MAX_FEATS = 100
-    #
-    #         # AoI + FOOTPRINTS = scenes
-    #         scenes = self.queryFootprints(self.retrievalUlx,
-    #                                       self.retrievalUly,
-    #                                       self.retrievalLrx,
-    #                                       self.retrievalLry,
-    #                                       self.retrievalSRS,
-    #                                       MAX_FEATS)
-    #
-    #         for scene in scenes:
-    #
-    #             evhrScene = EvhrScene()
-    #             evhrScene.request = self.request
-    #             evhrScene.sceneFile = scene
-    #             evhrScene.save()
-    #
-    #     return scenes
-                
     #---------------------------------------------------------------------------
     # getUtmSrs
     #
     # This method finds the UTM zone covering the most of the request's AoI.
     # It does this by finding the centroid of the AoI and choosing that zone.
     #---------------------------------------------------------------------------
-    def getUtmSrs(self, request):
-
-        # Centroid, called below, doesn't preserve the SRS.
-        srs = self.constructSrs(request.srs)
-        
-        center = self.bBoxToPolygon(request.ulx,
-                                    request.uly,
-                                    request.lrx,
-                                    request.lry,
-                                    srs).Centroid()
-        
-        # If request is already in WGS84 UTM...
-        if srs.IsProjected() and 'UTM' in srs.GetAttrValue('PROJCS'):
-            return request.srs
-
-        # If the center is not in geographic projection, convert it.
-        xValue = None
-
-        if not GeoRetriever.GEOG_4326.IsSame(srs):
-
-            xform = CoordinateTransformation(srs, GeoRetriever.GEOG_4326)
-            xPt = xform.TransformPoint(center.GetX(), center.GetY())
-            xValue = float(xPt.GetX())
-
-        else:
-            xValue = float(center.GetX())
-
-        # Initally, use the UTM zone of the upper-left corner of the AoI.
-        zone = (math.floor((xValue + 180.0) / 6) % 60) + 1
-        BASE_UTM_EPSG = '326'
-        epsg = int(BASE_UTM_EPSG + str(int(zone)))
-        srs = GeoRetriever.constructSrsFromIntCode(epsg)
-        return srs.ExportToWkt()
-                
-    #---------------------------------------------------------------------------
-    # listConstituents
-    #---------------------------------------------------------------------------
-    # def listConstituents(self):
+    # def getUtmSrs(self, request):
     #
-    #     # If a saved scene list exists. use it.
-    #     scenes = None
-    #     sceneFile = os.path.join(self.request.destination.name, 'scenes.txt')
+    #     # Centroid, called below, doesn't preserve the SRS.
+    #     srs = self.constructSrs(request.srs)
     #
-    #     if os.path.exists(sceneFile):
+    #     center = self.bBoxToPolygon(request.ulx,
+    #                                 request.uly,
+    #                                 request.lrx,
+    #                                 request.lry,
+    #                                 srs).Centroid()
     #
-    #         with open(sceneFile) as f: sceneString = f.read()
-    #         scenes = json.loads(sceneString)
+    #     # If request is already in WGS84 UTM...
+    #     if srs.IsProjected() and 'UTM' in srs.GetAttrValue('PROJCS'):
+    #         return request.srs
     #
-    #         if self.logger:
-    #             self.logger.info('Using saved scene list.')
+    #     # If the center is not in geographic projection, convert it.
+    #     xValue = None
+    #
+    #     if not GeoRetriever.GEOG_4326.IsSame(srs):
+    #
+    #         xform = CoordinateTransformation(srs, GeoRetriever.GEOG_4326)
+    #         xPt = xform.TransformPoint(center.GetX(), center.GetY())
+    #         xValue = float(xPt.GetX())
     #
     #     else:
+    #         xValue = float(center.GetX())
     #
-    #         # AoI + FOOTPRINTS = scenes
-    #         MAX_FEATS = 100
-    #
-    #         scenes = self.queryFootprints(self.retrievalUlx,
-    #                                       self.retrievalUly,
-    #                                       self.retrievalLrx,
-    #                                       self.retrievalLry,
-    #                                       self.retrievalSRS,
-    #                                       MAX_FEATS)
-    #
-    #         # Save the scenes because the query takes a long time to process.
-    #         jsonScenes = json.dumps(scenes)
-    #         with open(sceneFile, 'w+') as f: f.write(jsonScenes)
-    #
-    #     sceneGeoms = {}
-    #
-    #     for scene in scenes:
-    #
-    #         try:
-    #             dg = DgFile(scene, self.logger)
-    #
-    #         except Exception, e:
-    #
-    #             err             = EvhrError()
-    #             err.request     = self.request
-    #             err.inputFile   = scene
-    #             err.errorOutput = traceback.format_exc()
-    #             err.save()
-    #
-    #             continue
-    #
-    #         geom = self.bBoxToPolygon(dg.ulx, dg.uly, dg.lrx, dg.lry,dg.srs)
-    #         sceneGeoms[scene] = geom
-    #
-    #     # Define the tiles.
-    #     tiler = TilerHalfDegree(self.retrievalUlx,
-    #                             self.retrievalUly,
-    #                             self.retrievalLrx,
-    #                             self.retrievalLry,
-    #                             self.retrievalSRS,
-    #                             self.logger)
-    #
-    #     grid  = tiler.defineGrid()
-    #     tiles = tiler.gridToPolygons(grid)
-    #
-    #     # Tiles + scenes = constituents.
-    #     constituents = {}
-    #     tileNum = 0
-    #
-    #     for tile in tiles:
-    #
-    #         tileNum += 1
-    #         tileFile = self.createEmptyTile(tile, self.retrievalSRS, tileNum)
-    #         constituents[tileFile] = []
-    #
-    #         for scene in scenes:
-    #
-    #             # Scenes could be rejected due to missing information.
-    #             if not scene in sceneGeoms:
-    #                 continue
-    #
-    #             if not tile.GetSpatialReference(). \
-    #                    IsSame(sceneGeoms[scene].GetSpatialReference()):
-    #
-    #                 raise RuntimeError('Tile and scene must be in the '
-    #                                    'same SRS.')
-    #
-    #             if tile.Intersects(sceneGeoms[scene]):
-    #                 constituents[tileFile].append(scene)
-    #
-    #     return constituents
-        
+    #     # Initally, use the UTM zone of the upper-left corner of the AoI.
+    #     zone = (math.floor((xValue + 180.0) / 6) % 60) + 1
+    #     BASE_UTM_EPSG = '326'
+    #     epsg = int(BASE_UTM_EPSG + str(int(zone)))
+    #     srs = GeoRetriever.constructSrsFromIntCode(epsg)
+    #     return srs.ExportToWkt()
+                
     #---------------------------------------------------------------------------
     # listConstituents
     #---------------------------------------------------------------------------
@@ -736,51 +560,6 @@ class EvhrMosaicRetriever(GeoRetriever):
                 
         return toaFinal
 
-    # #---------------------------------------------------------------------------
-    # # queryFootprints
-    # #
-    # # listConstituents -> getScenes -> queryFootprints
-    # #---------------------------------------------------------------------------
-    # def queryFootprints(self, ulx, uly, lrx, lry, srs, maxFeatures = None):
-    #
-    #     whereClause = '-where "'
-    #     first = True
-    #
-    #     for sensor in EvhrHelper.RUN_SENSORS:
-    #
-    #         if first:
-    #             first = False
-    #         else:
-    #             whereClause += ' OR '
-    #
-    #         whereClause += 'SENSOR=' + "'" + sensor + "'"
-    #
-    #     whereClause += '"'
-    #
-    #     features = self.clipShp(EvhrMosaicRetriever.FOOTPRINTS_FILE, \
-    #                             ulx, uly, lrx, lry, srs,             \
-    #                             whereClause)
-    #
-    #     # Put them into a list of (row, path) tuples.
-    #     nitfs = []
-    #     featureCount = 0
-    #
-    #     for feature in features:
-    #
-    #         featureCount += 1
-    #
-    #         if maxFeatures and featureCount > maxFeatures:
-    #             break
-    #
-    #         nitf = str(feature. \
-    #                    getElementsByTagName('ogr:S_FILEPATH')[0]. \
-    #                    firstChild. \
-    #                    data)
-    #
-    #         nitfs.append(nitf)
-    #
-    #     return nitfs
-    #
     #---------------------------------------------------------------------------
     # retrieveOne
     #
