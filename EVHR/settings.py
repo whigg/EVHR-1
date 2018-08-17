0321607c2398fe7468c1ea2d90d25da62dbb4ef9
a <a@gs619-rgillm1.local>
2018-08-17 16:08:47 -0400

    misc

--- EVHR/settings.py
+++ EVHR/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '9t8##+l+s9f*@9*+xaak5wdjm0xtd^t797lapu0y3-3wo4!k^@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['evhr102']
+ALLOWED_HOSTS = ['evhr101', 'evhr102']
 
 
 # Application definition
