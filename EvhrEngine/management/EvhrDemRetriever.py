112e02f71f67666f30de00d8d2612e23362b6398
a <a@gs619-rgillm1.local>
2018-08-20 11:14:36 -0400

    EvhrDemRetriever

---
+++ EvhrEngine/management/EvhrDemRetriever.py
@@ -0,0 +1,67 @@
+
+from EvhrEngine.management.EvhrHelper import EvhrHelper
+from GeoProcessingEngine.management.GeoRetriever import GeoRetriever
+
+#-------------------------------------------------------------------------------
+# class EvhrDemRetriever
+#-------------------------------------------------------------------------------
+class EvhrDemRetriever(GeoRetriever):
+
+    #---------------------------------------------------------------------------
+    # __init__
+    #---------------------------------------------------------------------------
+    def __init__(self, request, logger, numProcesses):
+
+        self.evhrHelper = EvhrHelper(logger)
+
+        # The output SRS must be UTM, regardless of what the user chooses.
+        request.outSRS = self.evhrHelper.getUtmSrs(request)
+        request.save(update_fields = ['outSRS'])
+
+        # Initialize the base class.
+        super(EvhrDemRetriever, self).__init__(request, logger, numProcesses)
+
+        #---
+        # GeoRetriever should always choose EPSG:4326 as the retrieval SRS
+        # because that is all that FOOTPRINTS knows.  Verify that.
+        #---
+        if not self.retrievalSRS.IsSame(GeoRetriever.GEOG_4326):
+            raise RuntimeError('Retrieval SRS must be geographic.')
+
+    #---------------------------------------------------------------------------
+    # getEndPointSRSs
+    #---------------------------------------------------------------------------
+    def getEndPointSRSs(self, endPoint):
+        return [GeoRetriever.GEOG_4326]
+
+    #---------------------------------------------------------------------------
+    # listConstituents
+    #---------------------------------------------------------------------------
+    def listConstituents(self):
+
+        pairsOnly = True
+        
+        scenes = self.evhrHelper.getScenes(self.request,
+                                           self.retrievalUlx,
+                                           self.retrievalUly,
+                                           self.retrievalLrx,
+                                           self.retrievalLry,
+                                           self.retrievalSRS,
+                                           pairsOnly)
+
+        constituents = {}
+        
+        for scene in scenes:
+            
+            import pdb
+            pdb.set_trace()
+
+        return constituents
+
+    #---------------------------------------------------------------------------
+    # retrieveOne
+    #---------------------------------------------------------------------------
+    def retrieveOne(self, constituentFileName, fileList):
+
+        pass
+        
