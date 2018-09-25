c178453121d567d7261168c509df3fadd17911d4
a <a@gs619-rgillm1.local>
2018-09-25 12:25:25 -0400

    EDR3

--- EvhrEngine/management/EvhrDemRetriever.py
+++ EvhrEngine/management/EvhrDemRetriever.py
@@ -56,7 +56,7 @@ class EvhrDemRetriever(GeoRetriever):
                                                  lry, 
                                                  srs, 
                                                  request,
-                                                 true)
+                                                 True)
 
         pairs = []
 
