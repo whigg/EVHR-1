7f2d16c13f325c24bca79055861475ae1ca86284
a <a@gs619-rgillm1.local>
2018-10-08 14:51:07 -0400

    EDR3

--- EvhrEngine/management/EvhrDemRetriever.py
+++ EvhrEngine/management/EvhrDemRetriever.py
@@ -100,8 +100,8 @@ class EvhrDemRetriever(GeoRetriever):
                               
         # ONLY RUN ONE SCENE FOR TESTING
         # pairs = list(pairs)[:1]
-        pairs = ['WV02_20160630_1030010058056300_1030010057266900'] # works
-        print '*** ONLY RUNNING SCENE ' + str(pairs[0])
+        # pairs = ['WV02_20160630_1030010058056300_1030010057266900'] # works
+        # print '*** ONLY RUNNING SCENE ' + str(pairs[0])
 
         # Create the constituents.
         constituents = {}
