f44c4ee3f65d8a72b05ff030b818e3d19505a28d
a <a@gs619-rgillm1.local>
2018-11-15 10:49:42 -0500

    EDR5

---
+++ EvhrEngine/management/Footprints.py
@@ -0,0 +1,58 @@
+
+import os
+import tempfile
+from xml.dom import minidom
+
+from django.conf import settings
+
+from EvhrEngine.management.FootprintsScene import FootprintsScene
+from EvhrEngine.management.SystemCommand import SystemCommand
+
+#-------------------------------------------------------------------------------
+# class Footprints
+#-------------------------------------------------------------------------------
+class Footprints(object):
+
+    BASE_QUERY = 'ogr2ogr -f "GML" --debug on '                                       + \
+    
+    #---------------------------------------------------------------------------
+    # __init__
+    #---------------------------------------------------------------------------
+    def __init__(self, footprintsFile=settings.FOOTPRINTS_FILE, logger=None):
+        
+        # Verify the existence of Footprints.  You never know.
+        if not os.path.exists(footprintsFile):
+            
+            raise RuntimeError('Footprints file, ' + \
+                               footprintsFileE + \
+                               ' does not exist.')
+                               
+        self.footprintsFile = footprintsFile
+        self.logger = logger
+    
+    #---------------------------------------------------------------------------
+    # _query
+    #---------------------------------------------------------------------------
+    def _query(self, cmd):
+        
+        queryResult = tempfile.mkstemp()[1]
+        cmd += ' "' + queryResult + '"  "' + self.footprintsFile + '" '
+        SystemCommand(cmd, None, self.logger, None, True)
+        return minidom.parse(tempClipFile)
+        
+    #---------------------------------------------------------------------------
+    # sceneFromNtf
+    #---------------------------------------------------------------------------
+    def sceneFromNtf(self, ntfPath):
+    
+        if not ntfPath:
+            raise RuntimeError('An NITF file must be specified.')
+            
+        if not os.path.exists(ntfPath):
+            raise RuntimeError('NITF file, ' + str(ntfPath) +' does not exist.')
+
+        cmd = Footprints.BASEQUERY + '-where "S_FILEPATH=' + "'" + ntfPath+"'\""
+        gml = self._query(cmd)
+        scene = FootprintsScene(gml)
+        
+        return scene      
