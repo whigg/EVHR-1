a76e3cfcd1fd4b197ea0411d00ef64f4d1b0162d
a <a@gs619-rgillm1.local>
2019-02-27 11:26:40 -0500

    misc

---
+++ EvhrEngine/management/commands/compareDemRequests.py
@@ -0,0 +1,44 @@
+
+import glob
+import os
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    #---------------------------------------------------------------------------
+    # add_arguments
+    #---------------------------------------------------------------------------
+    def add_arguments(self, parser):
+
+        parser.add_argument('requestDir1', nargs='1')
+        parser.add_argument('requestDir2', nargs='1')
+
+    #---------------------------------------------------------------------------
+    # handle
+    #---------------------------------------------------------------------------
+    def handle(*args, **options):
+
+        reqDir1 = options['requestDir1']
+        reqDir2 = options['requestDir2']
+        
+        if not os.path.exists(reqDir1):
+
+            raise RuntimeError('Request directory, ' + 
+                               reqDir1 + 
+                               ' does not exist.')
+        
+        if not os.path.exists(reqDir2):
+
+            raise RuntimeError('Request directory, ' + 
+                               reqDir2 + 
+                               ' does not exist.')
+        
+        reqDir1 = os.path.join(reqDir1, 'dems')
+        reqDir2 = os.path.join(reqDir2, 'dems')
+
+        # Check for the same pairs.
+        req1PairDirs = [f if os.path.isdir(f) for f in os.path.listdir(reqDir1)]
+        print (str(req1PairDirs))
+        
+        
