7fb75474d305d6fbf6554a0c87c933442888664e
Roger Gill <>
2019-05-22 15:35:25 -0400

    misc

--- EvhrEngine/management/commands/tifToShapefile.py
+++ EvhrEngine/management/commands/tifToShapefile.py
@@ -12,22 +12,22 @@ from EvhrEngine.management.GdalFile import GdalFile
 #-------------------------------------------------------------------------------
 class Command(BaseCommand):
     
-    #---------------------------------------------------------------------------
-    # add_arguments
-    #---------------------------------------------------------------------------
-    def add_arguments(self, parser):
-
-        parser.add_argument('-f', help = 'Full path to file.')
+     #---------------------------------------------------------------------------
+     # add_arguments
+     #---------------------------------------------------------------------------
+     def add_arguments(self, parser):
         
- #---------------------------------------------------------------------------
- # handle
- #---------------------------------------------------------------------------
- def handle(*args, **options):
-
-     gdalFile = GdalFile(options['f'])         
-     outDir = tempfile.mkdtemp()
-     shapeFile = os.path.join(gridDir, os.path.basename(fileName) + '.shp')
-     outDriver = ogr.GetDriverByName('ESRI Shapefile')
-     dataSource = outDriver.CreateDataSource(shapeFile)
-     rts.filesToFeatures('tif', [fileName], gdalFile.srs, dataSource)
+         parser.add_argument('-f', help = 'Full path to file.')
+ 
+     #---------------------------------------------------------------------------
+     # handle
+     #---------------------------------------------------------------------------
+     def handle(*args, **options):
+     
+         gdalFile = GdalFile(options['f'])
+         outDir = tempfile.mkdtemp()
+         shapeFile = os.path.join(gridDir, os.path.basename(fileName) + '.shp')
+         outDriver = ogr.GetDriverByName('ESRI Shapefile')
+         dataSource = outDriver.CreateDataSource(shapeFile)
+         rts.filesToFeatures('tif', [fileName], gdalFile.srs, dataSource)
          
