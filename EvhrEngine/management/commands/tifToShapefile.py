a1537206df905e7c1434c2bb166ee831e35270a8
Roger Gill <>
2019-05-22 15:33:44 -0400

    misc

---
+++ EvhrEngine/management/commands/tifToShapefile.py
@@ -0,0 +1,33 @@
+
+import os
+import tempfile
+
+from django.core.management.base import BaseCommand
+
+from EvhrEngine.management.commands.requestToShapeFile import Command as rts
+from EvhrEngine.management.GdalFile import GdalFile
+
+#-------------------------------------------------------------------------------
+# class Command
+#-------------------------------------------------------------------------------
+class Command(BaseCommand):
+    
+    #---------------------------------------------------------------------------
+    # add_arguments
+    #---------------------------------------------------------------------------
+    def add_arguments(self, parser):
+
+        parser.add_argument('-f', help = 'Full path to file.')
+        
+ #---------------------------------------------------------------------------
+ # handle
+ #---------------------------------------------------------------------------
+ def handle(*args, **options):
+
+     gdalFile = GdalFile(options['f'])         
+     outDir = tempfile.mkdtemp()
+     shapeFile = os.path.join(gridDir, os.path.basename(fileName) + '.shp')
+     outDriver = ogr.GetDriverByName('ESRI Shapefile')
+     dataSource = outDriver.CreateDataSource(shapeFile)
+     rts.filesToFeatures('tif', [fileName], gdalFile.srs, dataSource)
+         
