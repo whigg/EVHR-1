df1b96efdbb31312dd301c20bf883a82b3d640e9
Roger Gill <>
2019-06-18 09:09:18 -0400

    rework-EMR

--- EvhrEngine/management/EvhrMosaicRetriever.py
+++ EvhrEngine/management/EvhrMosaicRetriever.py
@@ -133,40 +133,6 @@ class EvhrMosaicRetriever(GeoRetriever):
 
         return demName
 
-    # #---------------------------------------------------------------------------
-    # # createEmptyTile
-    # #---------------------------------------------------------------------------
-    # def createEmptyTile(self, tileGeometry, srs, tileNum):
-    #
-    #     ulx = tileGeometry.GetGeometryRef(0).GetPoint(0)[0]
-    #     uly = tileGeometry.GetGeometryRef(0).GetPoint(0)[1]
-    #     lrx = tileGeometry.GetGeometryRef(0).GetPoint(2)[0]
-    #     lry = tileGeometry.GetGeometryRef(0).GetPoint(2)[1]
-    #
-    #     tileName = os.path.join(self.tileDir, 'tile' + str(tileNum) + '.tif')
-    #
-    #     height = 1  # Choose a nominal height and width.  All we really
-    #     width  = 1  # need is the extent and file name of this tile tif.
-    #     driver = gdal.GetDriverByName('GTiff')
-    #     ds     = driver.Create(tileName, width, height)
-    #
-    #     if not ds:
-    #         raise RuntimeError('Unable to open ' + str(tileName))
-    #
-    #     ds.SetProjection(srs.ExportToWkt())
-    #
-    #     rotation = 0
-    #     xRes = lrx - ulx
-    #     yRes = (uly - lry) * -1.0
-    #
-    #     ds.SetGeoTransform([ulx, xRes, rotation, uly, rotation, yRes])
-    #     raster = numpy.zeros((height, width), dtype = numpy.uint8)
-    #
-    #     ds.GetRasterBand(1).WriteArray(raster)
-    #     ds = None
-    #
-    #     return tileName
-
     #---------------------------------------------------------------------------
     # deleteFiles
     #---------------------------------------------------------------------------
@@ -262,103 +228,6 @@ class EvhrMosaicRetriever(GeoRetriever):
         sceneFiles.sort()
         
         return sceneFiles
-        
-    # #---------------------------------------------------------------------------
-    # # listConstituents
-    # #---------------------------------------------------------------------------
-    # def listConstituents(self):
-    #
-    #     # Query for scenes.
-    #     scenes = self.getScenes(self.request,
-    #                             self.retrievalUlx,
-    #                             self.retrievalUly,
-    #                             self.retrievalLrx,
-    #                             self.retrievalLry,
-    #                             self.retrievalSRS)
-    #
-    #     if not scenes:
-    #         if self.logger:
-    #             self.logger.error('There were no level 1B scenes.')
-    #
-    #     #---
-    #     # Create a polygon for each scene.  They are used to test for
-    #     # intersection below.
-    #     #---
-    #     sceneGeoms = {}
-    #
-    #     for scene in scenes:
-    #
-    #         try:
-    #             dg = DgFile(scene, self.logger)
-    #
-    #         except Exception, e:
-    #
-    #             err             = EvhrError()
-    #             err.request     = self.request
-    #             err.inputFile   = scene
-    #             err.errorOutput = traceback.format_exc()
-    #             err.save()
-    #
-    #             continue
-    #
-    #         geom = self.bBoxToPolygon(dg.ulx, dg.uly, dg.lrx, dg.lry,dg.srs)
-    #         sceneGeoms[scene] = geom
-    #
-    #     # Define the tiles.
-    #     tiler = TilerHalfDegree(self.retrievalUlx,
-    #                             self.retrievalUly,
-    #                             self.retrievalLrx,
-    #                             self.retrievalLry,
-    #                             self.retrievalSRS,
-    #                             self.logger)
-    #
-    #     grid  = tiler.defineGrid()
-    #     tiles = tiler.gridToPolygons(grid)
-    #
-    #     # Tiles + scenes = constituents.
-    #     constituents = {}
-    #     tileNum = 0
-    #
-    #     for tile in tiles:
-    #
-    #         tileNum += 1
-    #         tileFile = self.createEmptyTile(tile, self.retrievalSRS, tileNum)
-    #         constituents[tileFile] = []
-    #
-    #         for scene in scenes:
-    #
-    #             # Scenes could be rejected due to missing information.
-    #             if not scene in sceneGeoms:
-    #                 continue
-    #
-    #             if not tile.GetSpatialReference(). \
-    #                    IsSame(sceneGeoms[scene].GetSpatialReference()):
-    #
-    #                 if self.logger:
-    #                     msg = 'Scene {} SRS is different from tile SRS'.format(scene)
-    #                     self.logger.warning(msg)
-    #                 continue
-    #
-    #             if tile.Intersects(sceneGeoms[scene]):
-    #
-    #                 constituents[tileFile].append(scene)
-    #
-    #                 if hasattr(settings, 'MAX_SCENES_PER_TILE') and \
-    #                    len(constituents[tileFile]) >= \
-    #                        settings.MAX_SCENES_PER_TILE:
-    #
-    #                        break
-    #
-    #         # Ensure the tile has scenes covering it. If it doesn't, delete
-    #         if not constituents[tileFile]:
-    #
-    #             if self.logger:
-    #                 msg = 'There were no scenes covering tile {}'.format(tile)
-    #                 self.logger.warning(msg)
-    #
-    #             del constituents[tileFile]
-    #
-    #     return constituents
         
     #---------------------------------------------------------------------------
     # listConstituents
@@ -651,59 +520,13 @@ class EvhrMosaicRetriever(GeoRetriever):
                   format(stripBandFile.replace('.r100.tif', ''), bandScenesStr)
 
             sCmd = SystemCommand(cmd, stripBandFile, self.logger, self.request,
-                                 True, True)
+                                 True, False)
                 
             DgFile(stripBandFile).setBandName(bandName)                          
             stripBandList.append(stripBandFile) 
 
         # Return the list of band strips
         return stripBandList
-
-    # #---------------------------------------------------------------------------
-    # # retrieveOne
-    # #
-    # # This receives a 1/2 degree tile file and the list of NITF files that
-    # # intersect it.  The NITF files have not been clipped.
-    # #---------------------------------------------------------------------------
-    # def retrieveOne(self, constituentFileName, fileList):
-    #
-    #     #---
-    #     # Mosaic scenes into strips, orthorectify the full strip, clip to the
-    #     # half-degree-square tile, and covert to Geotiff.
-    #     #---
-    #
-    #     completedStrips = []
-    #
-    #     # Get list of unique strip names for scenes in AOI
-    #     stripNameList = list(set([DgFile(scene).getStripName() \
-    #                               for scene in fileList]))
-    #
-    #     tileName = \
-    #             os.path.splitext(os.path.basename(constituentFileName))[0]
-    #
-    #     # For each strip, extract bands --> mosaic, ortho, toa each band
-    #     for stripName in stripNameList:
-    #
-    #         stripScenes = [scene for scene in fileList \
-    #                        if DgFile(scene).getStripName() == stripName]
-    #
-    #         stripBandList = self.scenesToStrip(stripName, stripScenes,
-    #                                                          tileName)
-    #
-    #         completedStrips.append(self.processStrip(stripName,
-    #                                                  stripBandList,
-    #                                                  tileName))
-    #
-    #     # self.deleteFiles(self.bandDir)
-    #     # self.deleteFiles(self.stripDir)
-    #     # self.deleteFiles(self.demDir)
-    #     # self.deleteFiles(self.orthoDir)
-    #
-    #     # Mosaic the scenes into a single file.
-    #
-    #     # Transform the mosaic into the output SRS.
-    #
-    #     return completedStrips[0]
 
     #---------------------------------------------------------------------------
     # retrieveOne
