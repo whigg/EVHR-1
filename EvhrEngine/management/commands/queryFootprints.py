ce25c0ef2e317ebb0d66bce8e812f2951f4d3f21
Roger Gill <>
2019-08-02 09:42:36 -0400

    surfR2

--- EvhrEngine/management/commands/queryFootprints.py
+++ EvhrEngine/management/commands/queryFootprints.py
@@ -45,7 +45,7 @@ class Command(BaseCommand):
             
             ulx, uly, lrx, lry, epsg = options['aoi']
             srs = SpatialReference()
-            srs.ImportFromEPSG(epsg)
+            srs.ImportFromEPSG(int(epsg))
             fpq.addAoi(ulx, uly, lrx, lry, srs)
             
         if options['catIDs']:
